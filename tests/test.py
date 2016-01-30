159b69403be1e3c9869c8fe4d2b235230789ffe3
marcel-veselka <marcel.veselka@tesena.com>
2016-01-30 20:12:03 +0100

    refactoring

--- tests/test.py
+++ tests/test.py
@@ -6,10 +6,10 @@ Tests are run by giving a path to the tests to be executed as an argument to
 this script. Possible Robot Framework options are given before the path.
 
 Examples:
-  test.py login_tests                        # Run all tests in a directory
-  test.py login_tests/valid_login.text       # Run tests in a specific file
-  test.py --variable BROWSER:IE login_tests  # Override variable
-  test.py -v BROWSER:IE -v DELAY:0.25 login_tests
+  tests.py login_tests                        # Run all tests in a directory
+  tests.py login_tests/valid_login.text       # Run tests in a specific file
+  tests.py --variable BROWSER:IE login_tests  # Override variable
+  tests.py -v BROWSER:IE -v DELAY:0.25 login_tests
 
 By default tests are executed with Firefox browser, but this can be changed
 by overriding the `BROWSER` variable as illustrated above. Similarly it is
@@ -53,9 +53,9 @@ def print_help():
     print __doc__
 
 def print_usage():
-    print 'Usage: test.py [options] datasource'
-    print '   or: test.py demoapp start|stop'
-    print '   or: test.py help'
+    print 'Usage: tests.py [options] datasource'
+    print '   or: tests.py demoapp start|stop'
+    print '   or: tests.py help'
 
 
 if __name__ == '__main__':
